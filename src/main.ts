c25eccf2890c587305633f5cee435fbfd5a7d15a
yamau <yamauzy@gmail.com>
2019-03-28 00:45:52 +0900

    create main.ts

---
+++ src/main.ts
@@ -0,0 +1,80 @@
+import { BrowserWindow, app, App } from "electron";
+import installExtension, {
+  REACT_DEVELOPER_TOOLS
+} from "electron-devtools-installer";
+
+const isDev = process.env["NODE_ENV"] === "development";
+
+class ElectronTest {
+  mainWindow: BrowserWindow | null = null;
+  app: App;
+
+  constructor(app: App) {
+    this.app = app;
+    this.app.on("window-all-closed", this.onWindowAllClosed.bind(this));
+    this.app.on("ready", this.create.bind(this));
+    this.app.on("activate", this.onActivated.bind(this));
+  }
+
+  onWindowAllClosed(): void {
+    this.app.quit();
+    return;
+  }
+
+  create(): void {
+    this.mainWindow = new BrowserWindow({
+      width: 800,
+      height: 800,
+      minWidth: 500,
+      minHeight: 200,
+      acceptFirstMouse: true,
+      titleBarStyle: "hidden"
+    });
+
+    if (isDev) {
+      // Load index.html via webpack dev server.
+      require("./webpack-server.js");
+      this.mainWindow.loadURL("http://localhost:3000/index.html");
+
+      // Open the DevTools.
+      this.mainWindow.webContents.openDevTools();
+    } else {
+      // Load index.html from the file system.
+      this.mainWindow.loadFile("dist/index.html");
+    }
+
+    // this.mainWindow.loadURL(
+    //   isDev
+    //     ? "http://localhost:3000"
+    //     : `file://${path.join(__dirname, "dist/index.html")}`
+    // );
+
+    this.mainWindow.on("closed", () => {
+      this.mainWindow = null;
+    });
+    return;
+  }
+
+  onReady(): void {
+    if (isDev) {
+      installExtension(REACT_DEVELOPER_TOOLS)
+        .then((name: string) => {
+          console.log(`Added Extension:  ${name}`);
+          this.create();
+        })
+        .catch((err: string) => console.log("An error occurred: ", err));
+    } else {
+      this.create();
+    }
+    return;
+  }
+
+  onActivated(): void {
+    if (this.mainWindow === null) {
+      this.create();
+    }
+    return;
+  }
+}
+
+const MyApp: ElectronTest = new ElectronTest(app);
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,19 +1,42 @@
 import * as webpack from "webpack";
+import * as path from "path";
 const MODE = "development";
+const SRC_DIR = path.resolve(__dirname, "src");
+const defaultInclude = [SRC_DIR];
 
 const mainConfig: webpack.Configuration = {
   mode: MODE,
+  target: "electron-main",
   entry: "./src/main.ts",
   output: {
     filename: "main.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)$/,
+        use: [{ loader: "ts-loader" }],
+        include: defaultInclude
+      }
+    ]
   }
 };
 
 const rendererConfig: webpack.Configuration = {
   mode: MODE,
+  target: "electron-renderer",
   entry: "./src/renderer.tsx",
   output: {
     filename: "renderer.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)$/,
+        use: [{ loader: "ts-loader" }],
+        include: defaultInclude
+      }
+    ]
   }
 };
 
